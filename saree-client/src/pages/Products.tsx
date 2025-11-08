546bf605cae6b86a5a8aeef6cbac8dbac7e62408
Sai Rama Doddi <sairama1026@gmail.com>
2025-11-08 22:41:58 +0530

    removed react import

--- saree-client/src/pages/Products.tsx
+++ saree-client/src/pages/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
